381d0b7a3b13c8439575fc0e3c9f81688cb2ed02
Matt <matthias.heerens@gmail.com>
2019-01-02 23:47:24 +0100

    update

---
+++ config.py
@@ -0,0 +1,5 @@
+# columns to be loaded from original csv, for training and testing
+SELECTED_COLUMNS = ["HasDetections",
+                    "AVProductStatesIdentifier",
+                    "AVProductsInstalled"
+                    ]
